d6e73f45722fe9bc42a5d43c850b59102d42b262
Cheng Taosheng <ctscy@163.com>
2013-04-14 13:18:58 +0800

    third commit

--- Sample/Sample/urls.py
+++ Sample/Sample/urls.py
@@ -15,4 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     # add something to commit
+    # add something to commit for third step
+    
 )
